d646ce2d02f63283ac6c87ba088304492488a27c
Teemur <yusupov-rr22@mail.ru>
2023-02-15 23:33:16 +0700

    task1

--- Task1.java
+++ Task1.java
@@ -4,16 +4,20 @@ import java.util.Deque;
 
 public class Task1 {
     // Дан Deque состоящий из последовательности цифр.
-// Необходимо проверить, что последовательность цифр является палиндромом
-public static void main(String[] args) {
-    Deque<Integer> deque = new ArrayDeque<>(Arrays.asList(1,2,3,4,5,6));
+    // Необходимо проверить, что последовательность цифр является палиндромом
+    public static void main(String[] args) {
+        Deque<Integer> deque = new ArrayDeque<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        boolean t = true;
+        for(int i=0;i<a.length;i++)
+            if(a[i]!=a[a.length-i-1])t=false;
+        System.out.println(t?"Это полиндром ":" Это не полиндром");
+ 
+        
+    }
 
-}
+    public boolean checkOn(Deque<Integer> deque) {
 
+        return false;
+    }
 
-public boolean checkOn(Deque<Integer> deque){
-
-    return false;
-}
-    
 }
